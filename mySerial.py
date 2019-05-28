3108cf2e1f69b5f0581618f00757e5c8b90a2a8b
william <wushuai@frogshealth.com>
2019-05-28 19:32:08 +0800

    增加配置lora功能

--- mySerial.py
+++ mySerial.py
@@ -12,70 +12,48 @@ import operator
 import crc
 import socket
 
-serial_name = None
-serial_baud = None
-serial_databits = None
-serial_stopbits = None
-serial_parity = None
 serial_fd = None
+LoraParamter = []
 
 def getDevList():
     return list(port_list.comports())
 
-def getSerialName(name):
-    global serial_name
-    name_list = name.split(' - ')
-    serial_name = name_list[0]
-    print(serial_name)
-
-def getSerialBaud(baud):
-    global serial_baud
-    serial_baud = int(baud, 10)
-    print(serial_baud)
-    
-def getSerialDatabits(dbits):
-    global serial_databits
-    serial_databits = int(dbits, 10)
-    print(serial_databits)
-    
-def getSerialStopbits(sbits):
-    global serial_stopbits
-    serial_stopbits = int(sbits, 10)
-    print(serial_stopbits)
-    
-def getSerialParity(parity):
-    global serial_parity
-    ret = ''
-    if operator.eq(parity, 'NONE'):
-      ret = serial.PARITY_NONE
-    elif operator.eq(parity, 'ODD'):
-      ret = serial.PARITY_ODD
-    elif operator.eq(parity, 'EVEN'):
-      ret = serial.PARITY_EVEN
-    else:
-      print('Parity Error')
-    serial_parity = ret
-    print(serial_parity)
-
-def open():
+def getSerialFD():
+  global serial_fd
+  return serial_fd
+  
+def open(paramter_list):
     global serial_fd 
-    global serial_name
-    global serial_baud 
-    global serial_parity
-    global serial_databits
-    global serial_stopbits
-    name = serial_name
-    baud = serial_baud
-    parity = serial_parity
-    dbits = serial_databits
-    sbits = serial_stopbits
     ret = False
+    
+    for val in paramter_list:
+      if val.startswith('name='):
+        val_list = val.split('=')
+        name = val_list[1]
+      if val.startswith('baudrate='):
+        val_list = val.split('=')
+        baud = int(val_list[1], 10)
+      if val.startswith('bytesize='):
+        val_list = val.split('=')
+        bytesize = int(val_list[1], 10)
+      elif val.startswith('parity='):
+        val_list = val.split('=')
+        parity = val_list[1]
+        if operator.eq(parity, 'NONE'):
+          parity = serial.PARITY_NONE
+        elif operator.eq(parity, 'ODD'):
+          parity = serial.PARITY_ODD
+        elif operator.eq(parity, 'EVEN'):
+          parity = serial.PARITY_EVEN
+      elif val.startswith('stopbits='):
+        val_list = val.split('=')
+        stopbits = int(val_list[1], 10)
+        
     if serial_fd:
       print('serial already open')
       return
     try:
-      serial_fd = serial.Serial(name, baudrate=baud, bytesize=dbits, parity=parity, stopbits=sbits, timeout=1)
-      #self.serial = serial.Serial(name, baudrate=baud, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=1)
+      serial_fd = serial.Serial(port=name, baudrate=baud, bytesize=bytesize, parity=parity, stopbits=stopbits, timeout=1)
       print('open serial: %s ' % serial_fd)
       ret = True
     except Exception as e:
@@ -104,7 +82,10 @@ def send(msgid, msgcmd, msgpar, msgidentify):
     send_head.append(msgid % 30)
     
     send_string.append(0xA5)
-    send_string.append(0x0C)
+    if msgcmd in [9, 11, 16]:
+      send_string.append(msgpar)
+    else:
+      send_string.append(0x0C)
     send_string.append(msgid & 0xff)
     send_string.append((msgid >> 8) & 0xff)
     send_string.append(msgcmd)
@@ -116,32 +97,66 @@ def send(msgid, msgcmd, msgpar, msgidentify):
     send_head.extend(send_string)
     
     crc_val = crc.calc_senddata(send_string)
-    print(hex(crc_val))
+    #print(hex(crc_val))
     send_head.append(crc_val & 0xff)
     send_head.append((crc_val >> 8) & 0xff)
     send_head.append(0x5A)
     
-    print('serial send:')
-    for val in send_head:
+    bytes_send = bytes(send_head)
+    try:
+      print('serial send:')
+      for val in bytes_send:
         print(hex(val), end=' ')
-        serial_fd.write(bytes([val]))
-    print(" ")
-    serial_fd.flush()
-    
+      print(" ")
+      
+      #可能是因为lora模块自身的原因，
+      #如果字符之间间隔大了就会分成多帧发送，导致接收不到
+      serial_fd.write(bytes_send)
+    except Exception as e:
+      print(e.args)
+   
+   
+def resetSerial(paramter_list):
+  global serial_fd
+  global serial_name
+  close()
+  open(paramter_list)
+  #serial_fd = serial.Serial(serial_name, baudrate=9600, bytesize=8, parity='N', stopbits=1, timeout=1)
+  #print(serial_fd)
+  
+def getParamter():
+  global serial_fd
+  global LoraParamter
+  #setSerial()
+  send = [0xc1, 0xc1, 0xc1]
+  bytes_send = bytes(send)
+  serial_fd.write(bytes_send)
+  time.sleep(0.2)
+  return LoraParamter
+  
+def setParamter(str_list):
+  global serial_fd
+  #setSerial()
+  bytes_send = bytes(str_list)
+  serial_fd.write(bytes_send)
+  print('setParamter')
+  print(bytes_send)
+  
 #>>> a = 'aabbccddeeff'
 #>>> a_bytes = bytes.fromhex(a)   hex字符串转换为bytes
 #b'\xaa\xbb\xcc\xdd\xee\xff'      bytes转换为hex字符串
 #>>> aa = a_bytes.hex() 
 def StartRecvice():
     global serial_fd
-    p = threading.Thread(target = ReadDataThread, args = [serial_fd, ])
+    p = threading.Thread(target = ReadDataThread, args = [ ])
     p.setDaemon(True)
     p.start()
     
 #从串口接收数据，然后发送给主窗口
-def ReadDataThread(serialid):
-    print('ReadDataThread: %s ' % serialid)
-    #print(socketid)
+def ReadDataThread():
+    #print('ReadDataThread: %s ' % serialid)
+    global serial_fd
+    global LoraParamter
     msglen = 0
     ch_list = []
     timeout_count = 0
@@ -149,25 +164,34 @@ def ReadDataThread(serialid):
     socketid = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     ipport = ('127.0.0.1', 9876)
     socketid.connect(ipport)
-    
+    recvflag = None
     while True:
+      if serial_fd:
         try:
-            if serialid.isOpen():
-                num = serialid.inWaiting()
-                if num > 0:
-                  ch = serialid.read(1).hex()
-                  ch = int(ch, 16)
-                  #print('ch=%x' % ch)
-                  #print(type(ch))
-                  if msglen > 0:
-                      ch_list.append(ch)
-                      msglen = msglen + 1
-                  if ch == 0xa5 and msglen == 0:
-                      msglen = 1
-                      ch_list = [ch, ]
-                  #print(msglen)
-                  #节点接收到服务器控制命令，长度12
-                  #服务器接收到节点响应信息，长度13
+          if serial_fd.isOpen():
+              num = serial_fd.inWaiting()
+              if num > 0:
+                ch = serial_fd.read(1).hex()
+                ch = int(ch, 16)
+                #print('ch=%x' % ch)
+                #print(type(ch))
+                if msglen > 0:
+                    ch_list.append(ch)
+                    msglen = msglen + 1
+                if ch == 0xa5 and msglen == 0:
+                    msglen = 1
+                    ch_list = [ch, ]
+                    recvflag = 1
+                elif ch == 0xc0:
+                  #print('recv lora paramter')
+                  msglen = 1
+                  recvflag = 2
+                  LoraParamter = []
+                  ch_list = [ch, ]
+                #print(msglen)
+                #节点接收到服务器控制命令，长度12
+                #服务器接收到节点响应信息，长度13
+                if recvflag == 1:
                   if msglen == 12 or msglen == 13:
                       #print('msglen=%d' % msglen)
                       if ch == 0x5a:
@@ -176,9 +200,9 @@ def ReadDataThread(serialid):
                           check_crc.append(ch_list[i])
                         #print(check_crc)
                         crcval = crc.calc_senddata(check_crc)
-                        print('crcval=%x' % crcval)
+                        #print('crcval=%x' % crcval)
                         if crcval == ch_list[msglen-3] | (ch_list[msglen-2] << 8):
-                          print('crc right')
+                          #print('crc right')
                           mId = check_crc[2] | (check_crc[3] << 8)
                           mCmd = check_crc[4]
                           if msglen == 12:
@@ -202,78 +226,31 @@ def ReadDataThread(serialid):
                             sendstr = '%s-%x-%x-%x-%x' % (mType, mId, mCmd, mResp, mIdentify)
                             sendbytes = bytes(sendstr, encoding='utf-8')
                             socketid.send(sendbytes)
-                          msglen = 0
                         else:
-                          msglen = 0
                           print('crc error')
+                        msglen = 0
+                        recvflag = 0
                       if msglen == 13:
                         msglen = 0
-                time.sleep(0.01)
-            else:
-                print('child ReadDataThread exit')
-                return
+                        recvflag = 0
+                  #接收地锁信息解析结束
+                elif recvflag == 2:
+                  if msglen == 6:
+                    msglen = 0
+                    recvflag = 0
+                    #print('recv paramter end')
+                    #print(ch_list)
+                    LoraParamter.extend(ch_list)
+              time.sleep(0.01)
+          else:
+              print('child ReadDataThread exit')
+              time.sleep(0.1)
+              #return
         except Exception as e:
             print(e.args)
             time.sleep(1)
-
-
-def serial_data_process(serial):
-    inputs = [serial,]
-    outputs = []
-    msglen = 0
-    ch_list = []
-    timeout_count = 0
-    message_queue = {}
-    message_queue[serial] = queue.queue()
-    recv_count = 0
-    all_count = 0
-
-    
-    while True:
-        readable,writeable,exceptional = select.select(inputs, outputs, inputs, 1)
-        if not (readable or writeable or exceptional):
-            continue
-        for r in readable:
-            ch = r.read(1).encode('hex')
-            if ch == 'a5':
-                msglen = 1 
-            if msglen > 0:
-                ch_list.append(ch)
-                msglen = msglen + 1 
-            if msglen == 14: 
-                msglen = 0
-                timeout_count = 0
-                print(time.strftime('%H:%M:%S',time.localtime(time.time())), end=' ')
-                for val in ch_list:
-                    print(val, end=' '),
-                print('lost:', end= ' '),
-                print(all_count - recv_count)
-                recv_count = recv_count + 1
-                all_count = all_count + 1
-                ch_list = []
-        for w in writeable:
-            try:
-                msg = message_queue[w].get_nowait()
-            except Queue.Empty:
-                outputs.remove(w)
-            except Exception as e:
-                print(e.args)
-                if w in outputs:
-                    outputs.remove(w)
-            else:
-                try:
-                    send_id = msg[0]
-                    send_cmd = msg[1]
-                    send_buff = calc_senddata(send_id, send_cmd)
-                    print('serial send:', end = ' ')
-                    for val in send_buff:
-                        print(hex(val), end=' ')
-                        w.write(bytes([val]))
-                    print(" ")
-                    outputs.remove(w)
-                except Exception as e:
-                    print(e.args)
-
+      else:
+        time.sleep(0.1)
 
 if __name__ == '__main__':
     getSerialName('COM9 - system')
